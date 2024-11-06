84ec6d8218cd05adc5f8bdeca2c1923c7170f004
niketanand-7 <niketanand7@gmail.com>
2024-11-06 15:00:48 -0500

    added stored procedure, function, view

--- p4_dbProject.sql
+++ p4_dbProject.sql
@@ -83,3 +83,120 @@ BEGIN
     SELECT @interviewId AS InterviewId;
 END
 GO
+
+
+-- function 1
+-- Helps in calculating the length of an internship, which is valuable information for applicants to understand the time commitment required. It ensures consistency in how the duration is calculated and displayed throughout your application.
+CREATE FUNCTION fnGetInternshipDuration (@jobId INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @duration INT;
+
+    SELECT @duration = DATEDIFF(day, startDate, endDate)
+    FROM internship
+    WHERE jobId = @jobId;
+
+    RETURN @duration;
+END;
+GO
+
+-- function 2
+-- Allows you to track and display the number of applications a user has submitted. This can enhance user engagement by providing them with insights into their activity. It's also useful for administrative reporting and analytics.
+CREATE FUNCTION fnGetUserApplicationCount (@userEmail VARCHAR(255))
+RETURNS INT
+AS
+BEGIN
+    DECLARE @appCount INT;
+
+    SELECT @appCount = COUNT(*)
+    FROM application
+    WHERE email = @userEmail;
+
+    RETURN @appCount;
+END;
+GO
+
+-- function 3
+-- Simplifies the process of retrieving and displaying all benefits associated with a full-time job. By encapsulating this logic in a function, you avoid repeating code and ensure that all benefits are consistently presented to users.
+CREATE FUNCTION fnGetJobBenefits (@jobId INT)
+RETURNS VARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @benefits VARCHAR(MAX);
+
+    SELECT @benefits = 
+        STUFF((
+            SELECT ', ' + benefit
+            FROM fullTime_benefits
+            WHERE jobId = @jobId
+            FOR XML PATH(''), TYPE
+        ).value('.', 'NVARCHAR(MAX)'), 1, 2, '');
+
+    RETURN @benefits;
+END;
+GO
+
+-- view 1
+--
+CREATE VIEW vwJobPostings AS
+SELECT
+    j.jobId,
+    j.jobTitle,
+    c.companyId,
+    c.name AS CompanyName,
+    c.description AS CompanyDescription,
+    jl.location,
+    CASE
+        WHEN i.jobId IS NOT NULL THEN 'Internship'
+        WHEN f.jobId IS NOT NULL THEN 'Full-Time'
+        ELSE 'Other'
+    END AS JobType
+FROM job j
+INNER JOIN company c ON j.companyId = c.companyId
+LEFT JOIN job_location jl ON j.jobId = jl.jobId
+LEFT JOIN internship i ON j.jobId = i.jobId
+LEFT JOIN fullTime f ON j.jobId = f.jobId;
+GO
+
+
+-- view 2
+--
+CREATE VIEW vwUserApplications AS
+SELECT
+    a.applicationId,
+    a.email AS UserEmail,
+    u.name AS UserName,
+    a.status,
+    a.additionalNotes,
+    a.createdDate,
+    j.jobId,
+    j.jobTitle,
+    c.companyId,
+    c.name AS CompanyName
+FROM application a
+INNER JOIN [user] u ON a.email = u.email
+INNER JOIN job j ON a.jobId = j.jobId
+INNER JOIN company c ON j.companyId = c.companyId;
+GO
+
+-- view 3
+--
+CREATE VIEW vwFullTimeJobBenefits AS
+SELECT
+    j.jobId,
+    j.jobTitle,
+    c.companyId,
+    c.name AS CompanyName,
+    f.startDate,
+    f.salary,
+    fb.benefit
+FROM fullTime f
+INNER JOIN job j ON f.jobId = j.jobId
+INNER JOIN company c ON j.companyId = c.companyId
+LEFT JOIN fullTime_benefits fb ON f.jobId = fb.jobId;
+GO
+
+
+
+
